ef86646953e74ff00b1bf3ddabf0e6aaf56b7799
Bessam <sahlibessam@gmail.com>
2021-12-04 20:33:43 +0100

    Making the singleton Thread safe

--- src/tech/bsahli/creational/singleton/DBSingleton.java
+++ src/tech/bsahli/creational/singleton/DBSingleton.java
@@ -2,14 +2,22 @@ package tech.bsahli.creational.singleton;
 
 public class DBSingleton {
 
-    private static DBSingleton instance = null;
+    // the keyword volatile makes the singleton thread safe
+    private static volatile DBSingleton instance = null;
 
-    private DBSingleton(){} // to make it it impossible to create instances from outside the class
+    private DBSingleton() {// to make it it impossible to create instances from outside the class
+        if (instance != null) // in case of using reflection
+            throw new RuntimeException("Use getInstance() to create an Instance");
+    }
 
     public static DBSingleton getInstance() {
-
-        if(instance == null) // this will make the singleton lazily loaded
-            instance = new DBSingleton();
+        if (instance == null) {// this will make the singleton lazily loaded
+            synchronized (DBSingleton.class) { // sinchronize the class when we create an instance
+                // in case two threads try to create
+                if (instance == null)
+                    instance = new DBSingleton();
+            }
+        }
         return instance;
     }
 }
