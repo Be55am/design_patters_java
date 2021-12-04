f2cb14189588d5821297f564e94494914c3b39d4
Bessam <sahlibessam@gmail.com>
2021-12-04 20:22:18 +0100

    Making the singleton lazily loaded

--- src/tech/bsahli/creational/singleton/DBSingleton.java
+++ src/tech/bsahli/creational/singleton/DBSingleton.java
@@ -2,11 +2,14 @@ package tech.bsahli.creational.singleton;
 
 public class DBSingleton {
 
-    private static DBSingleton instance = new DBSingleton();
+    private static DBSingleton instance = null;
 
     private DBSingleton(){} // to make it it impossible to create instances from outside the class
 
     public static DBSingleton getInstance() {
+
+        if(instance == null) // this will make the singleton lazily loaded
+            instance = new DBSingleton();
         return instance;
     }
 }
